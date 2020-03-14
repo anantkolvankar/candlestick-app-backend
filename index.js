d8478c7135862fd0fab1ca87e62069e7a4d9f1f3
anantkolvankar <ani.kolvankar@gmail.com>
2020-03-14 23:53:26 +0530

    Added config file.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./src/app');
-
-var server = app.listen(8080, function () {
+const config = require('./src/config')
+var server = app.listen(config.app.port, function () {
    var port = server.address().port
    console.log("Candlestick api listening at http://localhost:%s", port)
 })
---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+ app: {
+   port: 8080
+ },
+ coindcx: {
+   public_url: 'https://public.coindcx.com',
+   api_url: 'https://api.coindcx.com'
+ }
+};
+module.exports = config;
